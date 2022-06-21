e0ac4a7281be698732733108ecb07f7c1f45c9d2
EmJ <emily@example.com>
2022-06-21 15:08:49 -0700

    create customer form functioning

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import CustomerForm from './Customer';
 
 function App() {
+
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="sales">
+            <Route path="" element={<CustomerForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
--- sales/api/sales_project/settings.py
+++ sales/api/sales_project/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 INSTALLED_APPS = [
     'sales_rest.apps.SalesRestConfig',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -35,8 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+CRONJOBS = ["* * * * *", "poll.poller.get_autos"]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
