7f8bf49b17f7c7816b27ed91b817ca7457c5dcb7
EmJ <emily@example.com>
2022-06-23 12:08:35 -0700

    added form submit success messages

--- ghi/app/src/Customer.js
+++ ghi/app/src/Customer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class CustomerForm extends React.Component {
     constructor(props){
@@ -7,6 +8,9 @@ class CustomerForm extends React.Component {
             name: '',
             address: '',
             phoneNumber: '',
+            success: 'd-none',
+            form: '',
+            message: '',
         };
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleAddressChange = this.handleAddressChange.bind(this);
@@ -19,6 +23,9 @@ class CustomerForm extends React.Component {
         const data = {...this.state}
         data.phone_number = data.phoneNumber;
         delete data.phoneNumber;
+        delete data.form;
+        delete data.message;
+        delete data.success;
         console.log("check submit data: ", data)
         const customerUrl = 'http://localhost:8090/api/customer/';
         console.log(customerUrl);
@@ -33,6 +40,13 @@ class CustomerForm extends React.Component {
         if (response.ok) {
             const newCustomer = await response.json();
             console.log(newCustomer);
+
+            const success = {
+                success: '',
+                form: 'd-none',
+                message: `${this.state.name} has been added successfully.`,
+            }
+            this.setState(success);
 
             const cleared = {
                 name: '',
@@ -65,23 +79,30 @@ class CustomerForm extends React.Component {
         return(
             <div className="row">
                 <div className="offset-3 col-6">
-                    <div className="shadow p-4 mt-4">
-                        <h1>Create a new Customer</h1>
-                        <form onSubmit={this.handleSubmitChange} id="create-location-form">
-                        <div className="form-floating mb-3">
-                            <input onChange={this.handleNameChange} value={this.state.name} placeholder="Full Name" required type="text" name="name" id="name" className="form-control"/>
-                            <label htmlFor="name">Name</label>
+                    <div className={this.state.success}>
+                        <div className="alert alert-success mt-3" role="alert">
+                            {this.state.message}
                         </div>
-                        <div className="form-floating mb-3">
-                            <input onChange={this.handleAddressChange} value={this.state.address} placeholder="Address" required type="text" name="address" id="address" className="form-control"/>
-                            <label htmlFor="address">Address</label>
+                    </div>
+                    <div className={this.state.form}>
+                        <div className="shadow p-4 mt-4">
+                            <h1>Create a new Customer</h1>
+                            <form onSubmit={this.handleSubmitChange} id="create-location-form">
+                            <div className="form-floating mb-3">
+                                <input onChange={this.handleNameChange} value={this.state.name} placeholder="Full Name" required type="text" name="name" id="name" className="form-control"/>
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input onChange={this.handleAddressChange} value={this.state.address} placeholder="Address" required type="text" name="address" id="address" className="form-control"/>
+                                <label htmlFor="address">Address</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="tel" onChange={this.handlePhoneNumberChange} value={this.state.phoneNumber} placeholder="Phone number" required type="text" name="phone_number" id="phone_number" className="form-control"/>
+                                <label htmlFor="phone_number">Phone number</label>
+                            </div>
+                            <button className="btn btn-success">Create</button>
+                            </form>
                         </div>
-                        <div className="form-floating mb-3">
-                            <input type="tel" onChange={this.handlePhoneNumberChange} value={this.state.phoneNumber} placeholder="Phone number" required type="text" name="phone_number" id="phone_number" className="form-control"/>
-                            <label htmlFor="phone_number">Phone number</label>
-                        </div>
-                        <button className="btn btn-success">Create</button>
-                        </form>
                     </div>
                 </div>
             </div>
--- ghi/app/src/SalesPerson.js
+++ ghi/app/src/SalesPerson.js
@@ -6,6 +6,10 @@ class SalesPersonForm extends React.Component {
         this.state = {
             name: '',
             employeeNumber: '',
+            success: 'd-none',
+            form: '',
+            error: 'd-none',
+            message: '',
         };
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleEmployeeNumberChange = this.handleEmployeeNumberChange.bind(this);
@@ -16,7 +20,11 @@ class SalesPersonForm extends React.Component {
         event.preventDefault();
         const data = {...this.state}
         data.employee_number = data.employeeNumber;
-        delete data.employeeNumber
+        delete data.employeeNumber;
+        delete data.form;
+        delete data.error;
+        delete data.message;
+        delete data.success;
         console.log("check submit data: ", data)
         const salesUrl = 'http://localhost:8090/api/salespeople/';
         console.log(salesUrl);
@@ -32,6 +40,14 @@ class SalesPersonForm extends React.Component {
             const newCustomer = await response.json();
             console.log(newCustomer);
 
+            const success = {
+                success: '',
+                form: 'd-none',
+                error: 'd-none',
+                message: `${this.state.name} has been added successfully.`,
+            }
+            this.setState(success);
+
             const cleared = {
                 name: '',
                 employeeNumber: '',
@@ -39,7 +55,14 @@ class SalesPersonForm extends React.Component {
             this.setState(cleared);
         }
         else {
+            const errorMessage = {
+                error: '',
+                message: 'Sorry, that employee number is already taken. Please try again with a different number.'
+            }
+
+            this.setState(errorMessage);
             console.log("what is happening")
+            console.log(this.state)
         }
     }
 
@@ -57,19 +80,31 @@ class SalesPersonForm extends React.Component {
         return(
             <div className="row">
                 <div className="offset-3 col-6">
-                    <div className="shadow p-4 mt-4">
-                        <h1>Create a new Sales Person</h1>
-                        <form onSubmit={this.handleSubmitChange} id="create-location-form">
-                        <div className="form-floating mb-3">
-                            <input onChange={this.handleNameChange} value={this.state.name} placeholder="Full Name" required type="text" name="name" id="name" className="form-control"/>
-                            <label htmlFor="name">Name</label>
+                    <div className={this.state.success}>
+                        <div className="alert alert-success mt-4" role="alert">
+                            {this.state.message}
                         </div>
-                        <div className="form-floating mb-3">
-                            <input onChange={this.handleEmployeeNumberChange} value={this.state.employeeNumber} placeholder="Employee Number" required type="text" name="employee_number" id="employee_number" className="form-control"/>
-                            <label htmlFor="employee_number">Employee Number</label>
+                    </div>
+                    <div className={this.state.error}>
+                        <div className="alert alert-danger mt-4" role="alert">
+                            {this.state.message}
                         </div>
-                        <button className="btn btn-success">Create</button>
-                        </form>
+                    </div>
+                    <div className={this.state.form}>
+                        <div className="shadow p-4 mt-4">
+                            <h1>Create a new Sales Person</h1>
+                            <form onSubmit={this.handleSubmitChange} id="create-location-form">
+                            <div className="form-floating mb-3">
+                                <input onChange={this.handleNameChange} value={this.state.name} placeholder="Full Name" required type="text" name="name" id="name" className="form-control"/>
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input onChange={this.handleEmployeeNumberChange} value={this.state.employeeNumber} placeholder="Employee Number" required type="text" name="employee_number" id="employee_number" className="form-control"/>
+                                <label htmlFor="employee_number">Employee Number</label>
+                            </div>
+                            <button className="btn btn-success">Create</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
